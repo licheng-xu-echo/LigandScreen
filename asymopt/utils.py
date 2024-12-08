bbdcc2453e09d7e0706b32c60e19c6c82810abc5
echo <licheng_xu@zju.edu.cn>
2024-12-08 09:30:01 +0800

    update 'ddG2ee' and 'ee2ddG' func

--- asymopt/utils.py
+++ asymopt/utils.py
@@ -1,4 +1,5 @@
 from rdkit import Chem
+import numpy as np
 def clear_ferr_chirality(smi):
     mol = Chem.MolFromSmiles(smi)
     atoms = mol.GetAtoms()
@@ -18,3 +19,41 @@ def clear_ada_chirality(smi):
             atom = mol.GetAtomWithIdx(match_idx[at_idx])
             atom.SetChiralTag(Chem.rdchem.ChiralType.CHI_UNSPECIFIED)
     return Chem.MolToSmiles(mol)
+
+def ee2ddG(ee,T):
+    '''
+    Transformation from ee to ΔΔG
+    Parameters
+    ----------
+    ee : ndarray
+        Enantiomeric excess.
+    T : ndarray or float
+        Temperature (K).
+    Returns
+    -------
+    ddG : ndarray
+        ΔΔG (kcal/mol).
+    '''
+    
+    ddG = np.abs(8.314 * T * np.log((1-ee)/(1+ee)))  # J/mol
+    ddG = ddG/1000/4.18            # kcal/mol
+    return ddG
+
+def ddG2ee(ddG,T):
+    '''
+    Transformation from ΔΔG to ee. 
+    Parameters
+    ----------
+    ddG : ndarray
+        ΔΔG (kcal/mol).
+    T : ndarray or float
+        Temperature (K).
+    Returns
+    -------
+    ee : ndarray
+        Absolute value of enantiomeric excess.
+    '''
+    
+    ddG = ddG*1000*4.18
+    ee = (1-np.exp(ddG/(8.314*T)))/(1+np.exp(ddG/(8.314*T)))
+    return np.abs(ee)
