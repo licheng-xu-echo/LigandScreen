cb95fdb6cc585543e9b7b96f383ffa67a7715724
echo <licheng_xu@zju.edu.cn>
2024-12-09 09:00:34 +0800

    upload virtual ligand scoring code

--- asymopt/ml.py
+++ asymopt/ml.py
@@ -57,3 +57,49 @@ def model_delta_pred(tgt_x,tgt_y,base_x,base_y,model,simi=False,dist_type='eucli
         return all_test_y,all_test_p,r2,mae,prsr
     else:
         return all_test_y,all_test_p
+    
+def model_delta_pred_virt(model,base_x,base_y,delta_x,delta_y,tgt_x,
+                          dist_type='cosine',topk=20,tgt_simi_desc=None,base_simi_desc=None):
+    dist_type = dist_type.lower()
+    base_model = deepcopy(model)
+    delta_model = deepcopy(model)
+
+    simi_data_idx_data_pt_map = {}
+    for data_idx in range(len(tgt_x)):
+        sel_tgt_x = tgt_x[data_idx].reshape(1,-1)
+        simi_data_idx = None
+        if tgt_simi_desc is None:
+            dist_pair = pairwise_distances(sel_tgt_x,base_x,metric=dist_type).sum(axis=0)
+        else:
+            sel_tgt_simi_desc = tgt_simi_desc[data_idx].reshape(1,-1)
+            dist_pair = pairwise_distances(sel_tgt_simi_desc,base_simi_desc,metric=dist_type).sum(axis=0)
+        if isinstance(topk,int):
+            simi_data_idx = np.argsort(dist_pair)[:topk]  
+        else:
+            std_dist_pair = (dist_pair-dist_pair.min())/(dist_pair.max()-dist_pair.min())
+            simi_data_idx = np.where(std_dist_pair<topk)[0]  
+            if len(simi_data_idx) <= 1:
+                simi_data_idx = np.argsort(dist_pair)[:50]
+
+        if simi_data_idx is None or len(simi_data_idx) <= 1:
+            return None
+        simi_data_idx = tuple(sorted(simi_data_idx.tolist()))
+        if not simi_data_idx in simi_data_idx_data_pt_map:
+            simi_data_idx_data_pt_map[simi_data_idx] = [data_idx]
+        else:
+            simi_data_idx_data_pt_map[simi_data_idx].append(data_idx)
+    print(f'[INFO] There are {len(simi_data_idx_data_pt_map)} time(s) different delta prediciton')
+
+    tgt_p = -np.ones(len(tgt_x))*9999  ## initialize
+    for simi_data_idx in simi_data_idx_data_pt_map:
+        simi_data_x = base_x[list(simi_data_idx)]
+        simi_data_y = base_y[list(simi_data_idx)]
+        sel_tgt_x = tgt_x[simi_data_idx_data_pt_map[simi_data_idx]]
+        base_model.fit(simi_data_x,simi_data_y)
+        delta_p = base_model.predict(delta_x)
+        delta_d = delta_y - delta_p
+        delta_model.fit(delta_x,delta_d)
+        sel_tgt_p = base_model.predict(sel_tgt_x) + delta_model.predict(sel_tgt_x)
+        tgt_p[simi_data_idx_data_pt_map[simi_data_idx]] = sel_tgt_p
+    
+    return tgt_p
